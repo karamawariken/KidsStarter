accd9f84af00d04dfe1349a61b4b3e50fea956c2
karamawariken <24karamawariken@gmail.com>
2017-06-29 20:02:49 +0900

    create show products

--- KidsStarter/FirstViewController.swift
+++ KidsStarter/FirstViewController.swift
@@ -7,17 +7,78 @@
 //
 
 import UIKit
+import Alamofire
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
+    // ステータスバーの高さ
+    let statusBarHeight = UIApplication.shared.statusBarFrame.height + 32
+    //jsonデータ用NSDictionary
+    var jsonparse1: NSDictionary!
+    var jsonparse2: NSDictionary!
+    var products: NSArray!
+    let apiUrl = "http://192.168.0.4:3000/api/v1/products/search"
+    let headers: HTTPHeaders = [
+        "Uid": String(describing: UserDefaults.standard.string(forKey: "Uid") as! String),
+        "Access-Token": String(describing: UserDefaults.standard.string(forKey: "Access-Token") as! String),
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        Alamofire.request(apiUrl, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: headers).responseJSON {response in
+            self.jsonparse1 = response.result.value as! NSDictionary
+            self.jsonparse2 = self.jsonparse1["data"] as! NSDictionary
+            self.products = self.jsonparse2["products"] as! NSArray
+        }
+        
+        
+        let tableView = UITableView()
+        tableView.frame = CGRect(
+            x: 0,
+            y: statusBarHeight,
+            width: self.view.frame.width,
+            height: self.view.frame.height - statusBarHeight
+        )
+        //Delegate設定
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+
+    /// セルの個数を指定するデリゲートメソッド（必須）
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    /// セルに値を設定するデータソースメソッド（必須）
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        cell.accessoryType = .detailButton
+        var i = 0
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + .seconds(3)) {
+            if (self.products != nil){
+                for elm in self.products{
+                    if (indexPath.row == i)  {
+                        let product_data = elm as? NSDictionary
+                        if product_data?["title"] as? String != nil{
+                            cell.textLabel?.text = product_data?["title"] as? String
+                        }
+                        if product_data?["username"] as? String != nil{
+                            cell.detailTextLabel?.text = product_data?["username"] as? String
+                        }
+                    }
+                    i += 1
+                }
+            }
+        }
+        return cell
     }
 
 
--- KidsStarter/LoginViewController.swift
+++ KidsStarter/LoginViewController.swift
@@ -33,26 +33,25 @@ class LoginViewController: UIViewController{
             result in
             switch result{
             case let .success( permission, declinePermission, token):
-                //print("token:\(token),\(permission),\(declinePermission)")
-                //self.signIn(provider: "Facebook", accsesstoken: token.authenticationToken)
                 let apiUrl = "http://192.168.0.4:3000/api/v1/auth/sign_in"
                 let headers: HTTPHeaders = [
                     "Fb-Access-Token": String(describing: token.authenticationToken),
                     ]
                 Alamofire.request(apiUrl, method: .post, parameters: nil, encoding: JSONEncoding.default, headers: headers).responseJSON {response in
-                    //print(response.result.value)
                     let jsonDict = response.result.value as! NSDictionary
                     let userdata = jsonDict["data"] as! NSDictionary
+                    
                     //通信用の、uidとacces_tokenを発行する
                     UserDefaults.standard.set(userdata["uid"] as! String, forKey: "Uid")
                     UserDefaults.standard.set(userdata["access_token"] as! String, forKey: "Access-Token")
+                    
                     //現在のアプリ利用者playerをこちらのログインでは、大人と認識する
                     //一般 :adult  子ども :child
                     UserDefaults.standard.set("adult", forKey: "Player")
                 }
                 //TODO: 各ボタンを押した後の処理を別funcで書きたい
                 let mainStoryboard = UIStoryboard.init(name: "Main", bundle: nil)
-                let firstView = mainStoryboard.instantiateInitialViewController()// as! FirstViewController
+                let firstView = mainStoryboard.instantiateInitialViewController()
                 self.present(firstView!, animated: true,completion: nil)
                 
             case let .failed(error):
